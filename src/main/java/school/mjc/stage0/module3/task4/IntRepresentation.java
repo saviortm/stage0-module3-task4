975aba1f3e0862d41e3b4b765d5b252b04fdc92a
saviortm <106533562+saviortm@users.noreply.github.com>
2023-09-28 11:54:29 +0500

    Done

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int charC = 99;
+        int charH = 104;
+        int charA = 97;
+        int charR = 114;
+        System.out.println((char) charC);
+        System.out.println((char) charH);
+        System.out.println((char) charA);
+        System.out.println((char) charR);
     }
 }
